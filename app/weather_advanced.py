782253dfec94214b8a635ff6554e6005c906274f
anselbrandt <ansel@anselbrandt.com>
2025-10-13 17:17:36 -0700

    Update advanced example

--- app/weather_advanced.py
+++ app/weather_advanced.py
@@ -7,7 +7,7 @@ import urllib.parse
 from dotenv import load_dotenv
 from httpx import AsyncClient
 from pydantic import BaseModel
-from pydantic_ai import Agent, RunContext
+from pydantic_ai import Agent, RunContext, ModelRetry
 from restate import Context
 import logfire
 import restate
@@ -50,15 +50,18 @@ async def get_lat_lng(ctx: RunContext[Deps], location_description: str) -> dict:
     loc = urllib.parse.quote(location_description)
 
     async def action():
-        r = await ctx.deps.client.get(
-            f"https://api.mapbox.com/geocoding/v5/mapbox.places/{loc}.json",
-            params=params,
-        )
-        r.raise_for_status()
-        data = r.json()
-        if features := data["features"]:
-            lng, lat = features[0]["center"]
-            return {lat: lat, lng: lng}
+        with logfire.span("calling geocoding API", params=params) as span:
+            r = await ctx.deps.client.get(
+                f"https://api.mapbox.com/geocoding/v5/mapbox.places/{loc}.json",
+                params=params,
+            )
+            r.raise_for_status()
+            data = r.json()
+            if features := data["features"]:
+                lng, lat = features[0]["center"]
+                return {lat: lat, lng: lng}
+            else:
+                raise ModelRetry("Could not find the location")
 
     return await ctx.deps.restate_context.run_typed("Getting lat/lng", action)
 
@@ -79,42 +82,44 @@ async def get_weather(ctx: RunContext[Deps], lat: float, lng: float) -> dict[str
     }
 
     async def fetch_weather():
-        r = await ctx.deps.client.get(
-            "https://api.tomorrow.io/v4/weather/realtime", params=params
-        )
-        r.raise_for_status()
-        data = r.json()
-        values = data["data"]["values"]
-        # https://docs.tomorrow.io/reference/data-layers-weather-codes
-        code_lookup = {
-            1000: "Clear, Sunny",
-            1100: "Mostly Clear",
-            1101: "Partly Cloudy",
-            1102: "Mostly Cloudy",
-            1001: "Cloudy",
-            2000: "Fog",
-            2100: "Light Fog",
-            4000: "Drizzle",
-            4001: "Rain",
-            4200: "Light Rain",
-            4201: "Heavy Rain",
-            5000: "Snow",
-            5001: "Flurries",
-            5100: "Light Snow",
-            5101: "Heavy Snow",
-            6000: "Freezing Drizzle",
-            6001: "Freezing Rain",
-            6200: "Light Freezing Rain",
-            6201: "Heavy Freezing Rain",
-            7000: "Ice Pellets",
-            7101: "Heavy Ice Pellets",
-            7102: "Light Ice Pellets",
-            8000: "Thunderstorm",
-        }
-        return {
-            "temperature": f'{values["temperatureApparent"]}°C',
-            "description": code_lookup.get(values["weatherCode"], "Unknown"),
-        }
+        with logfire.span("calling weather API", params=params) as span:
+            r = await ctx.deps.client.get(
+                "https://api.tomorrow.io/v4/weather/realtime", params=params
+            )
+            r.raise_for_status()
+            data = r.json()
+            span.set_attribute("response", data)
+            values = data["data"]["values"]
+            # https://docs.tomorrow.io/reference/data-layers-weather-codes
+            code_lookup = {
+                1000: "Clear, Sunny",
+                1100: "Mostly Clear",
+                1101: "Partly Cloudy",
+                1102: "Mostly Cloudy",
+                1001: "Cloudy",
+                2000: "Fog",
+                2100: "Light Fog",
+                4000: "Drizzle",
+                4001: "Rain",
+                4200: "Light Rain",
+                4201: "Heavy Rain",
+                5000: "Snow",
+                5001: "Flurries",
+                5100: "Light Snow",
+                5101: "Heavy Snow",
+                6000: "Freezing Drizzle",
+                6001: "Freezing Rain",
+                6200: "Light Freezing Rain",
+                6201: "Heavy Freezing Rain",
+                7000: "Ice Pellets",
+                7101: "Heavy Ice Pellets",
+                7102: "Light Ice Pellets",
+                8000: "Thunderstorm",
+            }
+            return {
+                "temperature": f'{values["temperatureApparent"]}°C',
+                "description": code_lookup.get(values["weatherCode"], "Unknown"),
+            }
 
     weather_response_fut = ctx.deps.restate_context.run_typed(
         "Fetching weather", fetch_weather
