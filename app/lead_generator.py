b5fc0f92f55e66ddeccb9ca11bf37440ac187a8a
anselbrandt <ansel@anselbrandt.com>
2025-10-15 13:18:08 -0700

    Add query_executor

--- app/lead_generator.py
+++ app/lead_generator.py
@@ -1,22 +1,31 @@
 from typing import List
 import json
+import os
 
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from pydantic_ai import Agent
 from restate import RunOptions
+from tavily import AsyncTavilyClient
 import logfire
 import restate
 
 from app.data.example_prompt import example_prompt
 from app.restate import RestateAgent
-from app.schemas.lead_generator import LinkedInLeadQueries, SearchQuery
+from app.schemas.lead_generator import (
+    LinkedInLeadQueries,
+    SearchQuery,
+    TavilyResponse,
+    TavilyResult,
+)
 from app.system_prompts.lead_generator import (
     structured_instructions,
     unstructured_instructions,
 )
 
 load_dotenv()
+
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 
 logfire.configure(send_to_logfire="if-token-present")
 logfire.instrument_pydantic_ai()
@@ -98,7 +107,44 @@ async def run_lead_generator(ctx: restate.Context, prompt: Prompt) -> str:
             structured_output,
         )
 
-        with open("top_queries.json", "w", encoding="utf-8") as f:
-            json.dump(top_queries.model_dump(), f, indent=2)
+        # with open("top_queries.json", "w", encoding="utf-8") as f:
+        #     json.dump(top_queries.model_dump(), f, indent=2)
 
-        return top_queries.model_dump_json()
+        # return top_queries.model_dump_json()
+
+        class QueryResults(BaseModel):
+            query: str
+            description: str
+            results: List[TavilyResult]
+
+        async def query_executor_call(top_queries: TopQueries) -> QueryResults | None:
+            tavily_client = AsyncTavilyClient(api_key=TAVILY_API_KEY)
+            for q in top_queries.queries:
+                query = f"{q.query} site:linkedin.com"
+                raw_response = await tavily_client.search(
+                    query=query,
+                    include_raw_content=True,
+                    max_results=10,
+                    include_domains=["linkedin.com"],
+                )
+                response = TavilyResponse(**raw_response)
+                results = [
+                    result.model_dump()
+                    for result in response.results
+                    if "/in/" in result.url
+                ]
+                return QueryResults(
+                    query=q.query, description=q.description, results=results
+                )
+
+        leads: QueryResults = await ctx.run_typed(
+            "Query results",
+            query_executor_call,
+            RunOptions(max_attempts=3, type_hint=QueryResults),
+            top_queries,
+        )
+
+        with open("leads.json", "w", encoding="utf-8") as f:
+            json.dump(leads.model_dump(), f, indent=2)
+
+        return leads.model_dump_json()
